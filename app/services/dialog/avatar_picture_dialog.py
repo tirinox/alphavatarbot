c02a5739d7ab32aa200703df5da28f30b6bbb9fc
Maksim Koltsov <tirinox@gmail.com>
2021-02-10 23:02:54 +0300

    v2

--- app/services/dialog/avatar_picture_dialog.py
+++ app/services/dialog/avatar_picture_dialog.py
@@ -30,6 +30,7 @@ async def get_userpic(user: User) -> Image.Image:
 
 
 ALPHA_AVA_LOGO_PATH = './data/alpha-logo.png'
+ALPHA_FULL_BG_PATH = './data/alpha-avatar-v2.png'
 
 
 def image_square_crop(im):
@@ -72,6 +73,20 @@ def combine_frame_and_photo(cfg: Config, photo: Image.Image):
     logo = logo.resize((logo_size, logo_size)).convert('RGBA')
 
     photo.paste(logo, (logo_pos_x, logo_pos_y), mask=logo)
+
+    return photo
+
+
+@async_wrap
+def combine_frame_and_photo_v2(cfg: Config, photo: Image.Image):
+    photo = image_square_crop(photo)
+
+    photo_w, photo_h = photo.size
+    logo = Image.open(ALPHA_FULL_BG_PATH)
+
+    logo = logo.resize((photo_w, photo_w)).convert('RGBA')
+
+    photo.paste(logo, (0, 0), mask=logo)
 
     return photo
 
@@ -128,7 +143,8 @@ class AvatarDialog(BaseDialog):
                 await message.answer(loc.TEXT_AVA_ERR_SIZE, reply_markup=self.menu_kbd())
                 return
 
-            pic = await combine_frame_and_photo(self.deps.cfg, user_pic)
+            # pic = await combine_frame_and_photo(self.deps.cfg, user_pic)
+            pic = await combine_frame_and_photo_v2(self.deps.cfg, user_pic)
 
             user_id = message.from_user.id
             pic = img_to_bio(pic, name=f'alpha_avatar_{user_id}.png')
